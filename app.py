1536e4a1e2235b904cb9f8a9e8fa01a77c11e43d
meganet <meganet@MacBook-Air-de-meganet.local>
2025-11-11 23:38:43 -0300

    Ejecuta flask db upgrade automáticamente en todos los entornos

--- app.py
+++ app.py
@@ -33,12 +33,11 @@ def inicio():
 
 
 
-if __name__ == '__main__':
-    from flask_migrate import upgrade
-    with app.app_context():
-        upgrade()
-    app.run(debug=True)
+with app.app_context():
+    upgrade()
 
-if os.environ.get('RENDER') == 'true':
-    with app.app_context():
-        upgrade()
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
+    
