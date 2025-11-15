8bb400756d461466b03162cd1e670aca12c917a5
meganet <meganet@MacBook-Air-de-meganet.local>
2025-11-15 12:04:14 -0300

    Agrego print y manejo de errores en render_migrate.py para verificar migraciones

--- render_migrate.py
+++ render_migrate.py
@@ -1,10 +1,11 @@
 from app import app
 from flask_migrate import upgrade
 
-
 print("🔧 Ejecutando render_migrate.py en Render...")
 
-
-
 with app.app_context():
-    upgrade()
+    try:
+        upgrade()
+        print("✅ Migraciones aplicadas correctamente.")
+    except Exception as e:
+        print(f"❌ Error al aplicar migraciones: {e}")
